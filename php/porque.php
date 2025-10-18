ad117ade34b0c261fa43a4684e456317b0affe9f
Lelopes7 <malagolaleticia@gmail.com>
2025-10-18 16:50:29 -0300

    Base css pag porque

--- php/porque.php
+++ php/porque.php
@@ -44,8 +44,7 @@ session_start();
     <section class="section-main">
         <div>
             <h1>Porque nós?</h1>
-            <p style="font-family: Inter; color: rgb(194, 197, 170); font-weight: 500; font-size: 20px;">A TrashTracker
-                é a melhor escolha quando se trata de monitoramento de lixo. Somos diferentes do mercado convencional,
+            <p>A TrashTracker é a melhor escolha quando se trata de monitoramento de lixo. Somos diferentes do mercado convencional,
                 porque temos como foco nós conectar com as empresas e prefeituras, mas também com o dia a dia da
                 população. Viemos para ajudar você a economizar tempo!</p>
         </div>
@@ -58,9 +57,9 @@ session_start();
             <!--diferencial 1-->
             <div class="card">
                 <div class="card-texto">
-                    <h3 style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">
+                    <h3>
                     Acesso ao nível de lixo</h3>
-                    <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">
+                    <p >
                     Acesso a % de lixo de qualquer lixeira próxima a você, independente de onde você esteja!</p>
                  </div>
                  <div class="card-imagem-textura"></div>
@@ -68,62 +67,55 @@ session_start();
             <!--diferencial 2-->
             <div class="card">
                 <div class="card-texto">
-                <h3 style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">
-                    Dados estratégicos</h3>
-                <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">
-                    Dados estratégicos para que empresas e órgãos governamentais tenham mais produtividade na coleta!
-                </p>
+                <h3>Dados estratégicos</h3>
+                <p> Dados estratégicos para que empresas e órgãos governamentais tenham mais produtividade na coleta!</p>
                 </div>
                 <div class="card-imagem-textura"></div>
             </div>
             <!--diferencial 3-->
             <div class="card">
                 <div class="card-texto">
-                <p style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">
-                    Forúm</p>
-                <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">No
-                    forúm você pode deixar sugestões de melhorias, feedbacks e interagir com a comunidade!</p>
+                <h3>Forúm</h3>
+                <p>No forúm você pode deixar sugestões de melhorias, feedbacks e interagir com a comunidade!</p>
                 </div>
                 <div class="card-imagem-textura"></div>
             </div>
     </section>
 
     <!--dados-->
-    <section style="background-color: rgb(101, 109, 74); height: auto; width: auto; padding: 20px; ">
-        <h1 style="font-family: Inter; color: rgb(194, 197, 170); margin-left: 50px;">Dados</h1>
-        <div style="background-color: rgb(101, 109, 74); height: auto; width: auto; padding: 40px; display: flex;">
+    <section id="section-dados">
+        <h1>Dados</h1>
+        <div class="dados-container">
             <!--dado 1-->
-            <div
-                style="background: rgb(220, 218, 190); height: 350px; width: 450px; padding: 10px; margin-left: 100px; border-radius: 30px;">
-                <p style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">
-                    MENOS CO2 DISSIPADO</p>
-                <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">
+            <div class="dado-item co2">
+                <div class="dado-texto">
+                    <h3>MENOS CO2 DISSIPADO</h3>
+                    <p>
                     Cerca de 1 tonelada de CO2 a menos no meio ambiente!</p>
+                </div>
             </div>
             <!--dado 2-->
-            <div
-                style="background: rgb(220, 218, 190); height: 350px; width: 450px; padding: 10px; margin-left: 100px; border-radius: 30px;">
-                <p style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">MAIS
-                    ECONOMIA DE COMBUSTÍVEL</p>
-                <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">
+            <div class="dado-item combustivel">
+                <div class="dado-texto">
+                    <h3>MAIS ECONOMIA DE COMBUSTÍVEL</h3>
+                    <p>
                     Cerca de 375 litros de combustível economizados por mês! </p>
+                </div>
             </div>
             <!--dado 3-->
-            <div
-                style="background: rgb(220, 218, 190); height: 350px; width: 450px; padding: 10px; margin-left: 100px; border-radius: 30px;">
-                <p style="font-family: Inter; padding: 5px; margin-left: auto; font-size: 25px; font-weight: bold;">
-                    ROTAS MENORES</p>
-                <p style="font-family: Inter; margin-left: auto; padding: 5px; margin-bottom: auto; font-size: 22px;">
-                    Cerca de 936 quilometros a menos nas rotas da coleta!</p>
+            <div class="dado-item rotas">
+                <div class="dado-texto">
+                    <h3>ROTAS MENORES</h3>
+                    <p>Cerca de 936 quilometros a menos nas rotas da coleta!</p>
+                </div>    
             </div>
         </div>
     </section>
 
     <!--rodapé-->
-    <footer style="background-color: rgb(220, 218, 190); height: 80px; width: auto; padding: 5px;">
-        <img style="height: 30px; width: 30px; margin-top: 20px; margin-right: 10px; margin-left: auto;"
-            src="../images/trash.png">
-        <h2 style="font-family: Inter; color: rgb(65, 72, 51)">TrashTracker - Todos os direitos reservados ℗ </h2>
+    <footer class="footer">
+        <img src="../images/trash.png">
+        <h2>TrashTracker - Todos os direitos reservados ℗ </h2>
     </footer>
 
 </body>
