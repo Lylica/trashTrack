90b413c1bd7fd907f39ec255a0a4ab97d1030219
Lylica <aylla.aoliveira@gmail.com>
2025-10-20 18:27:13 -0300

    ajusta header da página de erro

--- php/erro.php
+++ php/erro.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de erro 404 personalizada do site TrashTracker</title>
-    <meta name="description" content="Página de erro 404 personalizada do site TrashTracker, utilizada para casos em que há erro de digitação, de busca ou de carregamento da página buscada">
+    <meta name="description"
+        content="Página de erro 404 personalizada do site TrashTracker, utilizada para casos em que há erro de digitação, de busca ou de carregamento da página buscada">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,26 +16,52 @@
 
     <!-- Media query -->
     <link rel="stylesheet" media="screen and (min-width: 480px) and (max-width: 960px)" href="erro.css" />
-    
+
     <!-- Meta Tags das redes sociais -->
     <meta property="og:title" content="Página de erro 404 personalizada do site TrashTracker">
     <meta property="og:description" content="Página de erro 404 personalizada do site TrashTracker, utilizada para casos em que há erro de digitação, de busca ou de carregamento da página buscada">
     <meta property="og:image" content="images/icone.png">
-    <meta property="og:url" content="https://srv1074333.hstgr.cloud/trashTrack/erro.html">
+    <meta property="og:url" content="https://srv1074333.hstgr.cloud/trashTrack/erro.php">
 
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
-    
+
     <!-- Google Analytics-->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-B7BYK41L1B"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag() { dataLayer.push(arguments); }
         gtag('js', new Date());
         gtag('config', 'G-B7BYK41L1B');
     </script>
 </head>
+
 <body>
-    
+    <header class="header-admin">
+        <a id="link-logo" href="index.php">
+            <img id="lata-lixo" src="../images/logo.png" alt="Logo">
+        </a>
+        <div class="header-title">
+            <h1 id="trashtracker">TrashTracker</h1>
+        </div>
+        <div class="nav">
+            <a class="menu-bar" href="index.php">INÍCIO</a>
+            <a class="menu-bar" href="sobre.php">SOBRE</a>
+            <a class="menu-bar" href="porque.php">PORQUE NÓS?</a>
+            <a class="menu-bar" href="dashboard.php">DASHBOARD</a>
+            <a class="menu-bar" href="forum.php">FORÚM</a>
+        </div>
+        <div class="header-user">
+            <?php if(isset($_SESSION['usuario'])): ?>
+            <img src="avatares/<?php echo htmlspecialchars($_SESSION['avatar']); ?>" alt="Avatar">
+            <span>Olá,
+            </span>
+            <a href="logout.php"><button id="btn-sair-header">Sair</button></a>
+            <?php else: ?>
+            <a href="login.php"><button id="btn-login-header">Login</button></a>
+            <?php endif; ?>
+        </div>
+    </header>
 </body>
+
 </html>
